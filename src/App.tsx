d2bc83db5caf93d4f9f1443288abb05effa45cd8
Vitaliy Morzhov <voxfoot@gmail.com>
2023-07-29 10:33:50 +0500

    start: implement working app for one file

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Uploader from './Uploader'
+
+function App() {
+
+  return (
+    <>
+        <h1>Uploader app</h1>
+        <Uploader />
+    </>
+  )
+}
+
+export default App
