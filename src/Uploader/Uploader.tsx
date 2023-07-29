0ebb32a20ec84561b7067c445e34df4859774af5
Vitaliy Morzhov <voxfoot@gmail.com>
2023-07-29 17:35:37 +0500

    inplement app

--- src/Uploader/Uploader.tsx
+++ src/Uploader/Uploader.tsx
@@ -8,6 +8,8 @@ const requesrtPath = 'https://cloud-api.yandex.net/v1/disk/resources/upload';
 
 const UploadFileToYandexDisk: React.FC = () => {
   const [selectedFile, setSelectedFile] = useState<File[]>([]);
+  const [count, setCount] = useState(0);
+  const [error, setError] = useState('');
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -15,25 +17,39 @@ const UploadFileToYandexDisk: React.FC = () => {
     }
   };
 
-  const handleFileUpload = useCallback( async () => {
-    if (selectedFile.length > 0) {
+  const handleFileUpload = useCallback(() => {
+    if (selectedFile.length > 0 && selectedFile.length < 100) {
         for (const file of selectedFile) {
             const formData = new FormData();
             formData.append('file', file);
 
-            const response = await axios.get(requesrtPath, { headers: { Authorization: `OAuth ${yandexDiskToken}`,}, params: { path: `${file.name}`, overwrite: true } });
-            const uploadUrl = response.data.href;
-
-            await axios.put(uploadUrl, formData);
+            axios
+                .get(requesrtPath,
+                    { headers: { Authorization: `OAuth ${yandexDiskToken}`}, params: { path: `${file.name}`, overwrite: true } })
+                .then(({ data: { href } }) => {
+                    axios.put(href, formData) })
+                .then(() => setCount((prev) => prev += 1))
         }
+    }
+
+    if (selectedFile.length === 0) {
+        setError('Добавьте файлы');
+    }
+
+    if (selectedFile.length > 100) {
+        setError('Можно загружать до 100 файлов')
     }
   }, [selectedFile]);
 
   return (
-    <div>
-      <input type="file" onChange={handleFileChange} multiple />
-      <button onClick={handleFileUpload}>Upload</button>
-    </div>
+    <main>
+        <div>
+            <input type="file" onChange={handleFileChange} multiple />
+            <button onClick={handleFileUpload}>Upload</button>
+        </div>
+        {count > 0 && <p>{`Загруженно файлов: ${count}`}</p>}
+        {error && <p>{error}</p>}
+    </main>
   );
 };
 
